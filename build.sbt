792da95121b76cfac637c910a27727491170bbd3
ssurendran <ssurendran@armor.com>
2018-09-02 18:19:36 +0530

    Implemented RDD writes to ElasticSearch and added configuration file

--- build.sbt
+++ build.sbt
@@ -9,7 +9,7 @@ val sparkVersion = "2.2.0"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-streaming" % sparkVersion,
-  "org.apache.spark" %% "spark-sql" % sparkVersion % "provided",
+  "org.apache.spark" %% "spark-sql" % sparkVersion ,
   "com.rabbitmq" % "amqp-client" % "3.6.6",
   "com.typesafe.akka" %% "akka-actor" % "2.5.3",
   "junit" % "junit" % "4.8.1" ,
@@ -20,3 +20,4 @@ libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 libraryDependencies += "com.stratio.receiver" % "spark-rabbitmq" % "0.5.1"
 libraryDependencies += "org.elasticsearch" %% "elasticsearch-spark-20" % "6.4.0"
+libraryDependencies += "com.typesafe" % "config" % "1.3.3"
--- src/main/scala/ApplicationTest.scala
+++ src/main/scala/ApplicationTest.scala
@@ -1,26 +1,38 @@
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession._
 import org.apache.spark.streaming.rabbitmq.RabbitMQUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object ApplicationTest {
   def main(args: Array[String]) {
-    //RabbitMQ things
-    val host = "localhost"
-    val port = "15672"
-    val queueName = "rabbitmq-queue"
-    val vHost = "/"
-    val userName = "clientsa"
-    val password = "J1Z43fYGUmq26DpwN"
-    val exchangeName = "test-exchangeName"
-    val exchangeType = "direct"
+
 
     // Set hadoop home directory for windows
     System.setProperty("hadoop.home.dir", "C:\\hadoop\\")
 
+    val value = ConfigFactory.load()
+
+    //RabbitMQ Configuration
+    val host = value.getString("appConf.rabbitMqConfig.host")
+    val port = value.getString("appConf.rabbitMqConfig.port")
+    val queueName = value.getString("appConf.rabbitMqConfig.queueName")
+    val vHost =value.getString("appConf.rabbitMqConfig.vHost")
+    val userName = value.getString("appConf.rabbitMqConfig.userName")
+    val password = value.getString("appConf.rabbitMqConfig.password")
+    val exchangeName =value.getString("appConf.rabbitMqConfig.exchangeName")
+    val exchangeType = value.getString("appConf.rabbitMqConfig.exchangeType")
+
     //Configure Spark properties
     val sparkConfig = new SparkConf()
       .setAppName("test")
-      .setIfMissing("spark.master", "local[*]")
+      .setIfMissing("spark.master",value.getString("appConf.sparkConf.masterMode"))
+      .set("es.index.auto.create", "true")
+      .set("es.nodes", value.getString("appConf.elasticConfig.nodes"))
+      .set("es.port", value.getString("appConf.elasticConfig.port"))
+      .set("es.http.timeout", value.getString("appConf.elasticConfig.httpTimeOut"))
+      .set("es.scroll.size", value.getString("appConf.elasticConfig.scrollSize"))
+
 
     sparkConfig.set("spark.driver.allowMultipleContexts", "true")
     sparkConfig.set("es.index.auto.create", "true")
@@ -29,7 +41,7 @@ object ApplicationTest {
     val ssc = new StreamingContext(sparkConfig, Seconds(5))
 
     // Open connection to RabbitMq and read stream
-    val receiverStream = RabbitMQUtils.createStream(ssc, Map(
+    val receiverRabbitMqStream = RabbitMQUtils.createStream(ssc, Map(
       "host" -> host,
       "port" -> port,
       "queueName" -> queueName,
@@ -40,17 +52,30 @@ object ApplicationTest {
       "password" -> password
     ))
 
+    //val sc = new SparkContext(sparkConfig)
+    val sqlContext = builder().config(sparkConfig).getOrCreate();
+
     val totalEvents = ssc.sparkContext.accumulator(0L, "Number of events received")
 
-    receiverStream.start()
+    receiverRabbitMqStream.start()
     println("started receiverStream")
 
-    receiverStream.foreachRDD(rdd => {
+    receiverRabbitMqStream.foreachRDD(rdd => {
       if (!rdd.isEmpty()) {
         val rdd1 = ssc.sparkContext.makeRDD(Seq(rdd.first()))
 
+        import sqlContext.implicits._
+        val validLine = rdd1.toDF()
+        import org.elasticsearch.spark.sql._
+
+        // **** Write to ElasticSearch ******
+        val indexName = value.getString("appConf.elasticConfig.indexName")
+        val indexType = value.getString("appConf.elasticConfig.indexType")
+        validLine.saveToEs(indexName + "/" + indexType)
+
         // For debug can write to local file
-         rdd1.saveAsTextFile("C:\\sparkscala\\straming")
+        //validLine.rdd.saveAsTextFile("C:\\sparkscala\\straming")
+
         val count = rdd.count()
         // Do something with this message
         println(s"EVENTS COUNT : \t $count")
